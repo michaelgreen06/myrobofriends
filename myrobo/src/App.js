e7473c8922eb66e316396060812c498765bf1f74
Michael Green <michaelgreen06@gmail.com>
2023-08-31 10:45:41 -0600

    working on cardlist

--- myrobo/src/App.js
+++ myrobo/src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
+import Cardlist from "./components/Cardlist";
+import { robots } from "./robots";
 
 function App() {
   return (
@@ -27,9 +29,21 @@ function App() {
         >
           Learn React
         </a>
+        <Cardlist robots={robots} />
       </header>
     </div>
   );
 }
 
 export default App;
+
+//I am going to use the problem solving framework I previously learned.
+//1). understand - know exactly what is being asked.
+//2). plan - “Given input X, what are the steps necessary to return output Y?”
+//3). divide - Break the problem down into the sub problems then solve the easiest one first
+
+// 1). I need to create a card for each user from the array of user objects. This is going to require doing a loop
+//over the array of objects. This will be done in the cardlist component. The cardlist component needs to take in a prop
+//of the users. It will then loop over the array of objects to make a card for each object
+//2). create the cardlist component w/o worry about where the actual props come from
+//3). sub problem is to make the component w/o worrying about where the array comes from
--- myrobo/src/components/Cardlist.js
+++ myrobo/src/components/Cardlist.js
@@ -1,1 +1,19 @@
+import React from "react";
+import Card from "./Card";
 
+const Cardlist = ({ robots }) => {
+  function loop() {
+    for (let i = 0; i < robots.length; i++) {
+      console.log(robots[i]);
+    }
+  }
+  return loop();
+};
+
+export default Cardlist;
+
+//1). Understand
+//2). Plan
+//3). Divide
+
+//8/31/23 left off w/ code being able to loop through the array from robots.js
